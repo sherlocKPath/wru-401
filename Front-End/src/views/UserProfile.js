ba178c8f75385ea35a1a2159bbdafec46200e365
sherlocKPath <143311874+sherlocKPath@users.noreply.github.com>
2024-11-28 02:00:50 +0700

    Update UserProfile.js

--- Front-End/src/views/UserProfile.js
+++ Front-End/src/views/UserProfile.js
@@ -1,228 +1,204 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import Swal from "sweetalert2";
-
-// react-bootstrap components
-import {
-  Badge,
-  Button,
-  Card,
-  Form,
-  Navbar,
-  Nav,
-  Container,
-  Row,
-  Col
-} from "react-bootstrap";
-
+import { Card, Form, Container, Row, Col } from "react-bootstrap";
+import { id } from "date-fns/locale";
 
 const User = () => {
+  const [employee, setEmployee] = useState({
+    name: "",
+    idcard: "",
+    email: "",
+    department: "",
+    phonenumber: "",
+    startWorkDate: "",
+    contractEndDate: "",
+    totalWorkDuration: "",
+    remainingContractDuration: "",
+    daywork:[{
+      date: "",
+      taskDetails: "",
+      progressLevel: "",
+      hoursWorked: "",
+    }]
+  });
 
-  const [employees, setEmployees] = useState([]);
-  
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const token = localStorage.getItem("token");
+
   useEffect(() => {
+    // ดึงข้อมูลโปรไฟล์ของพนักงาน
     axios
-      .get("http://localhost:50100/api/auth/getLists")
-      .then((res) => setEmployees(res.data))
-      .catch((err) => console.error(err));
-  }, []);
+      .get("http://localhost:50100/api/employees/user", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        withCredentials: true, // ถ้าคุณต้องการส่ง cookie
+      })
+      .then((res) => {
+        setEmployee(res.data); // ตั้งค่าข้อมูลพนักงานที่ได้จาก API
+        setIsLoading(false); // หยุดการโหลดเมื่อดึงข้อมูลเสร็จ
+      })
+      .catch((err) => {
+        console.error("Error fetching user profile:", err);
+        setError("ไม่สามารถดึงข้อมูลโปรไฟล์ได้");
+        setIsLoading(false);
+      });
+  }, [token]);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
 
   return (
-    <>
-      <Container fluid>
-        <Row>
-          <Col md="8">
-            <Card>
-              <Card.Header>
-                <Card.Title as="h4">Edit Profile</Card.Title>
-              </Card.Header>
-              <Card.Body>
-                <Form>
-                  <Row>
-                    <Col className="pr-1" md="5">
-                      <Form.Group>
-                        <label>Company (disabled)</label>
-                        <Form.Control
-                          defaultValue="Creative Code Inc."
-                          disabled
-                          placeholder="Company"
-                          type="text"
-                        ></Form.Control>
-                      </Form.Group>
-                    </Col>
-                    <Col className="px-1" md="3">
-                      <Form.Group>
-                        <label>Username</label>
-                        <Form.Control
-                          defaultValue="michael23"
-                          placeholder="Username"
-                          type="text"
-                        ></Form.Control>
-                      </Form.Group>
-                    </Col>
-                    <Col className="pl-1" md="4">
-                      <Form.Group>
-                        <label htmlFor="exampleInputEmail1">
-                          Email address
-                        </label>
-                        <Form.Control
-                          placeholder="Email"
-                          type="email"
-                        ></Form.Control>
-                      </Form.Group>
-                    </Col>
-                  </Row>
-                  <Row>
-                    <Col className="pr-1" md="6">
-                      <Form.Group>
-                        <label>First Name</label>
-                        <Form.Control
-                          defaultValue="Mike"
-                          placeholder="Company"
-                          type="text"
-                        ></Form.Control>
-                      </Form.Group>
-                    </Col>
-                    <Col className="pl-1" md="6">
-                      <Form.Group>
-                        <label>Last Name</label>
-                        <Form.Control
-                          defaultValue="Andrew"
-                          placeholder="Last Name"
-                          type="text"
-                        ></Form.Control>
-                      </Form.Group>
-                    </Col>
-                  </Row>
-                  <Row>
-                    <Col md="12">
-                      <Form.Group>
-                        <label>Address</label>
-                        <Form.Control
-                          defaultValue="Bld Mihail Kogalniceanu, nr. 8 Bl 1, Sc 1, Ap 09"
-                          placeholder="Home Address"
-                          type="text"
-                        ></Form.Control>
-                      </Form.Group>
-                    </Col>
-                  </Row>
-                  <Row>
-                    <Col className="pr-1" md="4">
-                      <Form.Group>
-                        <label>City</label>
-                        <Form.Control
-                          defaultValue="Mike"
-                          placeholder="City"
-                          type="text"
-                        ></Form.Control>
-                      </Form.Group>
-                    </Col>
-                    <Col className="px-1" md="4">
-                      <Form.Group>
-                        <label>Country</label>
-                        <Form.Control
-                          defaultValue="Andrew"
-                          placeholder="Country"
-                          type="text"
-                        ></Form.Control>
-                      </Form.Group>
-                    </Col>
-                    <Col className="pl-1" md="4">
-                      <Form.Group>
-                        <label>Postal Code</label>
-                        <Form.Control
-                          placeholder="ZIP Code"
-                          type="number"
-                        ></Form.Control>
-                      </Form.Group>
-                    </Col>
-                  </Row>
-                  <Row>
-                    <Col md="12">
-                      <Form.Group>
-                        <label>About Me</label>
-                        <Form.Control
-                          cols="80"
-                          defaultValue="Lamborghini Mercy, Your chick she so thirsty, I'm in
-                          that two seat Lambo."
-                          placeholder="Here can be your description"
-                          rows="4"
-                          as="textarea"
-                        ></Form.Control>
-                      </Form.Group>
-                    </Col>
-                  </Row>
-                  <Button
-                    className="btn-fill pull-right"
-                    type="submit"
-                    variant="info"
-                  >
-                    Update Profile
-                  </Button>
-                  <div className="clearfix"></div>
-                </Form>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col md="4">
-            <Card className="card-user">
-              <div className="card-image">
-                <img
-                  alt="..."
-                  src={require("assets/img/photo-1431578500526-4d9613015464.jpeg")}
-                ></img>
-              </div>
-              <Card.Body>
-                <div className="author">
-                  <a href="#pablo" onClick={(e) => e.preventDefault()}>
-                    <img
-                      alt="..."
-                      className="avatar border-gray"
-                      src={require("assets/img/faces/face-3.jpg")}
-                    ></img>
-                    <h5 className="title">Mike Andrew</h5>
-                  </a>
-                  <p className="description">michael24</p>
-                </div>
-                <p className="description text-center">
-                  "Lamborghini Mercy <br></br>
-                  Your chick she so thirsty <br></br>
-                  I'm in that two seat Lambo"
-                </p>
-              </Card.Body>
-              <hr></hr>
-              <div className="button-container mr-auto ml-auto">
-                <Button
-                  className="btn-simple btn-icon"
-                  href="#pablo"
-                  onClick={(e) => e.preventDefault()}
-                  variant="link"
-                >
-                  <i className="fab fa-facebook-square"></i>
-                </Button>
-                <Button
-                  className="btn-simple btn-icon"
-                  href="#pablo"
-                  onClick={(e) => e.preventDefault()}
-                  variant="link"
-                >
-                  <i className="fab fa-twitter"></i>
-                </Button>
-                <Button
-                  className="btn-simple btn-icon"
-                  href="#pablo"
-                  onClick={(e) => e.preventDefault()}
-                  variant="link"
-                >
-                  <i className="fab fa-google-plus-square"></i>
-                </Button>
-              </div>
-            </Card>
-          </Col>
-        </Row>
-      </Container>
-    </>
+    <Container fluid>
+      <Row>
+        <Col md="8">
+          <Card>
+            <Card.Header>
+              <Card.Title as="h4">ข้อมูลพนักงาน</Card.Title>
+            </Card.Header>
+            <Card.Body>
+              <Form>
+                <Row>
+                  <Col md="6">
+                    <Form.Group>
+                      <label>ชื่อ - นามสกุล</label>
+                      <Form.Control
+                        value={employee.name}
+                        placeholder="ชื่อ - นามสกุล"
+                        type="text"
+                        readOnly
+                      />
+                    </Form.Group>
+                  </Col>
+                  <Col md="6">
+                    <Form.Group>
+                      <label>เลขบัตรประชาชน</label>
+                      <Form.Control
+                        value={employee.idcard}
+                        placeholder="เลขบัตรประชาชน"
+                        type="text"
+                        readOnly
+                      />
+                    </Form.Group>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col md="6">
+                    <Form.Group>
+                      <label>อีเมล</label>
+                      <Form.Control
+                        value={employee.email}
+                        placeholder="แผนกที่สังกัด"
+                        type="text"
+                        readOnly
+                      />
+                    </Form.Group>
+                  </Col>
+                  <Col md="6">
+                    <Form.Group>
+                      <label>เบอร์โทรศัพท์</label>
+                      <Form.Control
+                        value={employee.phonenumber}
+                        placeholder="แผนกที่สังกัด"
+                        type="text"
+                        readOnly
+                      />
+                    </Form.Group>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col md="6">
+                    <Form.Group>
+                      <label>แผนกที่สังกัด</label>
+                      <Form.Control
+                        value={employee.department}
+                        placeholder="แผนกที่สังกัด"
+                        type="text"
+                        readOnly
+                      />
+                    </Form.Group>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col md="6">
+                    <Form.Group>
+                      <label>วันที่เริ่มทำงาน</label>
+                      <Form.Control
+                        value={
+                          employee.startWorkDate &&
+                          new Date(employee.startWorkDate).toLocaleDateString(
+                            "th-TH"
+                          )
+                        }
+                        placeholder="วันที่เริ่มงาน"
+                        type="text"
+                        readOnly
+                      />
+                    </Form.Group>
+                  </Col>
+                  <Col md="6">
+                    <Form.Group>
+                      <label>วันที่ครบสัญญาจ้าง</label>
+                      <Form.Control
+                        value={employee.contractEndDate &&
+                          new Date(employee.contractEndDate).toLocaleDateString(
+                            "th-TH"
+                          )}
+                        placeholder="วันที่ครบสัญญาจ้าง"
+                        type="text"
+                        readOnly
+                      />
+                    </Form.Group>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col md="6">
+                    <Form.Group>
+                      <label>อายุการทำงานรวม</label>
+                      <Form.Control
+                        value={employee.totalWorkDuration}
+                        placeholder="อายุการทำงานรวม"
+                        type="text"
+                        readOnly
+                      />
+                    </Form.Group>
+                  </Col>
+                  <Col md="6">
+                    <Form.Group>
+                      <label>จำนวนวันคงเหลือ</label>
+                      <Form.Control
+                        value={employee.remainingContractDuration}
+                        placeholder="จำนวนวันคงเหลือ"
+                        type="text"
+                        readOnly
+                      />
+                    </Form.Group>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col md="6">
+                  <label>การปฏิบัติงานรายวัน</label>
+                  {employee.daywork.map((daywork, index) => (
+                    <div key={index}>
+                      <Form.Control
+                        value={employee.daywork[index].taskDetails}
+                        placeholder="การปฏิบัติงานรายวัน"
+                        type="text"
+                        readOnly
+                      />
+                    </div>
+                  ))}
+                  </Col>
+                </Row>
+              </Form>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
   );
-}
+};
 
 export default User;
